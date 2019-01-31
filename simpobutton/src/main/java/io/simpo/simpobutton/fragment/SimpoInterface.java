4f0c3957dc91be79f2b66be8f39663ae702f9a04
Sergey Kurdyuk <sudorous@gmail.com>
2019-01-31 21:31:31 +0400

    Button click fixes

--- simpobutton/src/main/java/io/simpo/simpobutton/fragment/SimpoInterface.java
+++ simpobutton/src/main/java/io/simpo/simpobutton/fragment/SimpoInterface.java
@@ -3,9 +3,11 @@ package io.simpo.simpobutton.fragment;
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -36,6 +38,58 @@ final public class SimpoInterface extends DialogFragment {
         return simpoInterface;
     }
 
+    private boolean isRedirected;
+    private WebViewClient webViewClient = new WebViewClient() {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (url.equals("simpo://interface.close")) {
+                Simpo.close(getActivity());
+            }
+            isRedirected = true;
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity( intent );
+            return true;
+        }
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            super.onReceivedError(view, errorCode, description, failingUrl);
+
+            view.setVisibility(View.GONE);
+        }
+
+        ProgressDialog progressDialog;
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            isRedirected = false;
+        }
+
+        public void onLoadResource(WebView view, String url) {
+            if (!isRedirected) {
+                if (progressDialog == null && getActivity() != null) {
+                    progressDialog = new ProgressDialog(getActivity());
+                    progressDialog.setMessage("Loading...");
+                    progressDialog.show();
+                }
+            }
+
+        }
+
+        public void onPageFinished(WebView view, String url) {
+            isRedirected = true;
+
+            if (progressDialog != null && progressDialog.isShowing()) {
+                progressDialog.dismiss();
+                progressDialog = null;
+            }
+
+        }
+
+    };
+
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +106,7 @@ final public class SimpoInterface extends DialogFragment {
         webView.getSettings().setDomStorageEnabled(true);
         webView.setBackgroundColor(Color.TRANSPARENT);
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(webViewClient);
         webView.loadUrl(getArguments().getString(URL_ARG));
         return view;
     }
